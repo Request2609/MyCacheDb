bdb70744cb15c81f398d09e61dda6ecf945c0d82
buger723 <1433882609@qq.com>
2020-01-20 10:32:34 +0800

    熟悉

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int a ;
+    int b ;
+    cout << a << "   " << b << endl ;
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
+
